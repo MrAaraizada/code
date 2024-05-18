5f63253b35d39e153746f087052e6055e44f5229
Aditya Raizada <mr.aaraizada@gmail.com>
2024-05-19 01:34:00 +0530

    feat: implement e-commerce templates
    - Create product listing pages
    - Add shopping cart layouts
    - Implement checkout flow
    - Set up product detail views

---
+++ pages/templates/ecommerce/ProductGrid.tsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+interface ProductGridProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const ProductGrid: React.FC<ProductGridProps> = ({ children, className }) => {
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  );
+};
+
+export default ProductGrid;
---
+++ pages/templates/ecommerce/ShoppingCart.tsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+interface ShoppingCartProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const ShoppingCart: React.FC<ShoppingCartProps> = ({ children, className }) => {
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  );
+};
+
+export default ShoppingCart;
