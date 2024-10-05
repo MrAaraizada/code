8ca0331f908a78fd3cd017e6f8a1c350e7c90fb6
Aditya Raizada <mr.aaraizada@gmail.com>
2024-10-06 00:05:00 +0530

    feat: implement line and area charts
    - Create responsive line chart component
    - Add area chart with gradients
    - Implement multi-series support
    - Set up chart animation system

---
+++ library/components/charts/AnimationSystem.ts
@@ -0,0 +1,3 @@
+﻿export class ChartAnimationSystem {
+  static animate() {}
+}
---
+++ library/components/charts/AreaChart.tsx
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+
+export const AreaChart = () => {
+  return <div className='area-chart'>Area Chart</div>;
+};
