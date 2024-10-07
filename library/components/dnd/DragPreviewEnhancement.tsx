7e1cbb5df911562b39bd9e6ffe68e85d08b42059
Aditya Raizada <mr.aaraizada@gmail.com>
2024-10-08 02:30:00 +0530

    feat: enhance drag and drop features
    - Add drag preview customization
    - Implement drop zones styling
    - Create drag constraints system
    - Set up drag analytics

---
+++ library/components/dnd/DragPreviewEnhancement.tsx
@@ -0,0 +1,2 @@
+﻿import React from 'react';
+export const DragPreviewEnhancement = () => <div>Enhanced Preview</div>;
---
+++ library/components/dnd/DropZonesStyling.tsx
@@ -0,0 +1,2 @@
+﻿import React from 'react';
+export const DropZonesStyling = () => <div>Styled Drop Zone</div>;
