1891f265fd998cbd83c46ec19b49b39b8e249844
Aditya Raizada <mr.aaraizada@gmail.com>
2024-10-28 02:35:00 +0530

    feat: implement Android theme system
    - Create dynamic color system
    - Add theme customization
    - Implement dark mode support
    - Set up theme switching

---
+++ component/lib/DynamicColorSystem.kt
@@ -0,0 +1,9 @@
+﻿package com.example.lib
+
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DynamicColorSystem() {
+    val colorScheme = dynamicDarkColorScheme()
+}
