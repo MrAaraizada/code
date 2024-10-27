86b4ccd97bbabf430c74ba2256ea4397344d4023
Aditya Raizada <mr.aaraizada@gmail.com>
2024-10-28 04:35:00 +0530

    feat: add Android animation system
    - Create Material motion
    - Implement shared element transitions
    - Add gesture animations
    - Set up animation performance

---
+++ component/lib/GestureAnimations.kt
@@ -0,0 +1,5 @@
+﻿package com.example.lib
+
+class GestureAnimations {
+    fun animateGesture() {}
+}
---
+++ component/lib/SharedElementTransitions.kt
@@ -0,0 +1,5 @@
+﻿package com.example.lib
+
+class SharedElementTransitions {
+    fun createTransition() {}
+}
