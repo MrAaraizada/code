cf9f54232b7d1f37f45c3dc114417b3342e0a992
Aditya Raizada <mr.aaraizada@gmail.com>
2024-11-05 00:15:00 +0530

    feat(develop): add optimized Image component

---
+++ develop/components/Image/Image.tsx
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+import { Image as RNImage, StyleSheet } from 'react-native';
+
+interface ImageProps {
+  source: any;
+  style?: any;
+  resizeMode?: 'cover' | 'contain' | 'stretch';
+  width?: number;
+  height?: number;
+}
+
+export const Image: React.FC<ImageProps> = ({ 
+  source, style, resizeMode = 'cover', width, height 
+}) => {
+  const dynamicStyle = { ...(width && { width }), ...(height && { height }) };
+  return <RNImage source={source} style={[styles.image, dynamicStyle, style]} resizeMode={resizeMode} />;
+};
+
+const styles = StyleSheet.create({
+  image: { borderRadius: 8 },
+});
+
+export default Image;
