e5748b397b5cfed68e3c4c5d4e8b5e1a108944c1
Aditya Raizada <mr.aaraizada@gmail.com>
2024-11-07 03:17:00 +0530

    feat(develop): add SectionList with performance optimizations

---
+++ develop/components/SectionList/SectionList.tsx
@@ -0,0 +1,31 @@
+﻿import React from 'react';
+import { SectionList as RNSectionList, StyleSheet } from 'react-native';
+
+interface SectionListProps {
+  sections: any[];
+  renderItem: ({ item, index, section }: any) => React.ReactElement;
+  renderSectionHeader?: ({ section }: any) => React.ReactElement;
+  keyExtractor?: (item: any, index: number) => string;
+  style?: any;
+}
+
+export const SectionList: React.FC<SectionListProps> = ({ 
+  sections, renderItem, renderSectionHeader, keyExtractor, style 
+}) => {
+  return (
+    <RNSectionList
+      sections={sections}
+      renderItem={renderItem}
+      renderSectionHeader={renderSectionHeader}
+      keyExtractor={keyExtractor}
+      style={[styles.sectionList, style]}
+      showsVerticalScrollIndicator={false}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionList: { flex: 1 },
+});
+
+export default SectionList;
