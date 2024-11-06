15b297eb3a42991685288c40a0ef8099388efe8a
Aditya Raizada <mr.aaraizada@gmail.com>
2024-11-07 02:22:00 +0530

    feat(develop): implement optimized FlatList component

---
+++ develop/components/FlatList/FlatList.tsx
@@ -0,0 +1,34 @@
+﻿import React from 'react';
+import { FlatList as RNFlatList, StyleSheet } from 'react-native';
+
+interface FlatListProps {
+  data: any[];
+  renderItem: ({ item, index }: { item: any; index: number }) => React.ReactElement;
+  keyExtractor?: (item: any, index: number) => string;
+  horizontal?: boolean;
+  numColumns?: number;
+  style?: any;
+}
+
+export const FlatList: React.FC<FlatListProps> = ({ 
+  data, renderItem, keyExtractor, horizontal = false, numColumns = 1, style 
+}) => {
+  return (
+    <RNFlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor}
+      horizontal={horizontal}
+      numColumns={numColumns}
+      style={[styles.flatList, style]}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  flatList: { flex: 1 },
+});
+
+export default FlatList;
