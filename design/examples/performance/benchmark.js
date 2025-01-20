f4c063cbc5327f4ebfe1462c93b1c6d1dcb1c3fe
Aditya Raizada <mr.aaraizada@gmail.com>
2025-01-21 00:45:00 +0530

    feat(design/examples): add Material UI performance benchmark data

---
+++ design/examples/performance/benchmark.js
@@ -0,0 +1,19 @@
+
+// Material UI performance benchmarks
+const materialUIBenchmarks = {
+  bundleSize: {
+    core: '87KB',
+    withIcons: '156KB',
+    fullLibrary: '312KB',
+  },
+  renderTime: {
+    simpleButton: '0.8ms',
+    complexCard: '2.3ms',
+    dataTable: '15.7ms',
+  },
+  memoryUsage: {
+    initial: '2.1MB',
+    afterInteraction: '2.8MB',
+    peak: '4.2MB',
+  },
+};
