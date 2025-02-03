6f5c402de36bdd0affe61ef0b2f68b5acdd4b35b
Aditya Raizada <mr.aaraizada@gmail.com>
2025-02-04 01:50:00 +0530

    feat(design/examples): create advanced Remix Material UI integration with SSR and forms

--- design/examples/remix/RemixIntegration.tsx
+++ design/examples/remix/RemixIntegration.tsx
@@ -1,5 +1,9 @@
-﻿import React from 'react';
+import React from 'react';
 
 export const RemixIntegration = () => {
   return <div className='remix-integration'><h1>Remix</h1></div>;
 };
+
+// Advanced Remix + Material UI integration - Feb 4, 2025
+const remixFeatures = { ssr: true, forms: true, navigation: true };
+
