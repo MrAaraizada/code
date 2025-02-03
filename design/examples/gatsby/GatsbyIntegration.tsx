bc3dc1fa934f49c4d6892a75d6af495a6f01aa47
Aditya Raizada <mr.aaraizada@gmail.com>
2025-02-04 02:45:00 +0530

    feat(design/examples): enhance Gatsby Material UI integration with GraphQL and theme switching

--- design/examples/gatsby/GatsbyIntegration.tsx
+++ design/examples/gatsby/GatsbyIntegration.tsx
@@ -1,5 +1,9 @@
-﻿import React from 'react';
+import React from 'react';
 
 export const GatsbyIntegration = () => {
   return <div className='gatsby-integration'><h1>Gatsby</h1></div>;
 };
+
+// Enhanced Gatsby + Material UI integration - Feb 4, 2025
+const gatsbyFeatures = { graphql: true, ssg: true, plugins: true };
+
