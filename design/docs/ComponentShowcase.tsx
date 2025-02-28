5aeaedaaabec38777731c59d277bec9a7d3252a6
Aditya Raizada <mr.aaraizada@gmail.com>
2025-03-01 03:15:00 +0530

    feat(design/docs): update material UI v7 integration docs

--- design/docs/ComponentShowcase.tsx
+++ design/docs/ComponentShowcase.tsx
@@ -3,3 +3,5 @@
 export const ComponentShowcase = () => {
   return <div className='component-showcase'></div>;
 };
+
+// Updated: 2026-01-20 23:50:59 - feat(design/docs): update material UI v7 integration docs
--- design/docs/MaterialUIv7Integration.ts
+++ design/docs/MaterialUIv7Integration.ts
@@ -2,3 +2,5 @@
   static setupMUI() {}
   static configureTheme() { return {}; }
 }
+
+// Updated: 2026-01-20 23:50:59 - feat(design/docs): update material UI v7 integration docs
