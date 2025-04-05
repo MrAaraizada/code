25a47121bd2673e95eb2538e9cc1a73e7a3e37d2
Aditya Raizada <mr.aaraizada@gmail.com>
2025-04-06 01:50:00 +0530

    test(develop/testing): add accessibility test utilities

--- develop/testing/AccessibilityTestUtils.ts
+++ develop/testing/AccessibilityTestUtils.ts
@@ -115,3 +115,5 @@ export class AccessibilityTestUtils {
 }
 
 // Updated: 2026-01-20 23:51:07 - test(develop/testing): add accessibility testing utilities
+
+// Updated: 2026-01-21 00:00:53 - test(develop/testing): add accessibility test utilities
