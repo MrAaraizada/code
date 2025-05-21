15035328433fde77163e1318f5b277055af55ebe
Aditya Raizada <mr.aaraizada@gmail.com>
2025-05-22 03:05:00 +0530

    feat(pages/joy-ui): enhance card component page

--- pages/joy-ui/react-card.js
+++ pages/joy-ui/react-card.js
@@ -4,3 +4,5 @@ import * as pageProps from 'docs/data/joy/components/card/card.md?muiMarkdown';
 export default function Page() {
   return <MarkdownDocs {...pageProps} />;
 }
+
+// Updated: 2026-01-21 00:05:54 - feat(pages/joy-ui): enhance card component page
