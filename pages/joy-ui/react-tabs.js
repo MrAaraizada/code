d9c466b08dfe3c884145479e600aef5d7fb4db2d
Aditya Raizada <mr.aaraizada@gmail.com>
2025-05-22 04:40:00 +0530

    feat(pages/joy-ui): enhance tabs component page

--- pages/joy-ui/react-tabs.js
+++ pages/joy-ui/react-tabs.js
@@ -4,3 +4,5 @@ import * as pageProps from 'docs/data/joy/components/tabs/tabs.md?muiMarkdown';
 export default function Page() {
   return <MarkdownDocs {...pageProps} />;
 }
+
+// Updated: 2026-01-21 00:05:54 - feat(pages/joy-ui): enhance tabs component page
