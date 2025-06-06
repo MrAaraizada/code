4aec9fe28c0f6d33dcd856d54675e35d1fc373d8
Aditya Raizada <mr.aaraizada@gmail.com>
2025-06-07 04:20:00 +0530

    test(packages/tests): add screen reader tests

--- packages/test-utils/setup.ts
+++ packages/test-utils/setup.ts
@@ -32,3 +32,5 @@ export default setup;
 // Updated: 2026-01-21 00:00:51 - test(packages/test-utils): add test setup utilities
 
 // Updated: 2026-01-21 00:12:10 - test(packages/tests): add component installation tests
+
+// Updated: 2026-01-21 00:12:13 - test(packages/tests): add screen reader tests
