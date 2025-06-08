d905702a2f29fda6a9ac649ab607631e577fb931
Aditya Raizada <mr.aaraizada@gmail.com>
2025-06-09 04:50:00 +0530

    test(component/testing): add Android integration tests

--- component/testing/IntegrationTestingAndroid.kt
+++ component/testing/IntegrationTestingAndroid.kt
@@ -5,3 +5,5 @@ class IntegrationTestingAndroid {
 }
 
 // Updated: 2026-01-21 00:00:51 - test(component/testing): implement integration testing
+
+// Updated: 2026-01-21 00:12:16 - test(component/testing): add Android integration tests
