c88b141c969e1520ffeeba8d2df22c295fe1c2ed
Aditya Raizada <mr.aaraizada@gmail.com>
2025-06-17 03:20:00 +0530

    test(packages/tests): add desktop responsive tests

--- packages/test-utils/generators.ts
+++ packages/test-utils/generators.ts
@@ -32,3 +32,5 @@ export default generators;
 // Updated: 2026-01-21 00:01:01 - test(packages/test-utils): implement test generators
 
 // Updated: 2026-01-21 00:12:10 - test(packages/tests): add framework compatibility tests
+
+// Updated: 2026-01-21 00:12:21 - test(packages/tests): add desktop responsive tests
