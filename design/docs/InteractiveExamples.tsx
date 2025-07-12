3bc5be0db659897f75cd5bd3c4d7dc6f63e34838
Aditya Raizada <mr.aaraizada@gmail.com>
2025-07-13 04:00:00 +0530

    docs(design/docs): add icon documentation

--- design/docs/InteractiveExamples.tsx
+++ design/docs/InteractiveExamples.tsx
@@ -7,3 +7,5 @@ export const InteractiveExamples = () => {
 // Updated: 2026-01-20 23:51:15 - docs(design/docs): enhance documentation site structure
 
 // Updated: 2026-01-21 00:01:04 - docs(design/docs): add interactive examples
+
+// Updated: 2026-01-21 00:38:10 - docs(design/docs): add icon documentation
