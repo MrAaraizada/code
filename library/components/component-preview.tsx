93d7c3c0683c6ff3e5c2449e30af3ebb8ec5783d
Aditya Raizada <mr.aaraizada@gmail.com>
2025-07-13 03:20:00 +0530

    feat(library/components): add icon wrapper component

--- library/components/component-preview.tsx
+++ library/components/component-preview.tsx
@@ -178,3 +178,5 @@ export function ComponentPreview({
 // Updated: 2026-01-21 00:01:05 - docs(library/components): enhance component preview
 
 // Updated: 2026-01-21 00:12:14 - test(library/components): add input component tests
+
+// Updated: 2026-01-21 00:38:10 - feat(library/components): add icon wrapper component
