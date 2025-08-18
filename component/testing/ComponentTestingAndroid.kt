909cc96b740760d18852de639ad9e01909d47047
Aditya Raizada <mr.aaraizada@gmail.com>
2025-08-19 04:30:00 +0530

    feat(component/testing): optimize component testing

--- component/testing/ComponentTestingAndroid.kt
+++ component/testing/ComponentTestingAndroid.kt
@@ -9,3 +9,5 @@ class ComponentTestingAndroid {
 // Updated: 2026-01-21 00:12:16 - test(component/testing): add Android component tests
 
 // Updated: 2026-01-21 00:12:29 - docs(component/testing): add Android testing docs
+
+// Updated: 2026-01-21 00:41:28 - feat(component/testing): optimize component testing
