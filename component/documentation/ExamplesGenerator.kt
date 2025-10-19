272642b456aeb1cf4787f5544b17d035e85d9a9f
Aditya Raizada <mr.aaraizada@gmail.com>
2025-10-20 04:35:00 +0530

    feat(component): generate localized code examples

--- component/documentation/ExamplesGenerator.kt
+++ component/documentation/ExamplesGenerator.kt
@@ -179,3 +179,5 @@ fun ExamplesGeneratorPreview() {
 // Updated: 2026-01-21 00:01:08 - docs(component/documentation): implement examples generator
 
 // Updated: 2026-01-21 00:41:27 - feat(component/documentation): add examples generator
+
+// Modified: 2026-01-21 00:52:23
